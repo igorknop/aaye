c9119ca3ce9d4a043b0020b3e0ee77c2d2411699
Igor Knop <igorknop@gmail.com>
2019-12-28 15:44:45 -0300

    Split the code in more files

---
+++ aiye-game.js
@@ -0,0 +1,57 @@
+export const stats = {};
+
+export function createCard(card) {
+    stats[`cards`] = stats[`cards`] ? stats[`cards`] + 1 : 1;
+    const eCard = document.createElement("div");
+    eCard.classList.add("card");
+    eCard.classList.add("mini-euro");
+    eCard.classList.add(card.type);
+    eCard.classList.add(`mask${card.mask}`);
+    let eType = document.createElement("div");
+    eType.classList.add("type");
+    eType.classList.add(card.type);
+    eType.innerText = card.type;
+    eCard.appendChild(eType);
+    stats[`${card.type}`] = stats[`${card.type}`] ? stats[`${card.type}`] + 1 : 1;
+    let eCost = document.createElement("div");
+    eCost.classList.add("cost");
+    eCost.innerText = card.cost;
+    eCard.appendChild(eCost);
+    stats[`$${card.cost}`] = stats[`$${card.cost}`] ? stats[`$${card.cost}`] + 1 : 1;
+    let eExtra = document.createElement("div");
+    eExtra.classList.add("extra");
+    if (card.extra.length > 0 && card.extra[0] !== "initial") {
+        stats[`extras`] = stats[`extras`] ? stats[`extras`] + 1 : 1;
+
+    }
+    for (const key in card.extra) {
+        const item = card.extra[key];
+        let eItem = document.createElement("div");
+        eItem.classList.add("item");
+        eItem.classList.add(item);
+        eItem.innerText = item;
+        eExtra.appendChild(eItem);
+        if (item === "initial") {
+            stats[`${item}`] = stats[`${item}`] ? stats[`${item}`] + 1 : 1;
+        }
+    }
+    eCard.appendChild(eExtra);
+    let ePV = document.createElement("div");
+    ePV.classList.add("pv");
+    ePV.innerText = card.pv;
+    eCard.appendChild(ePV);
+    stats[`${card.pv}pv`] = stats[`${card.pv}pv`] ? stats[`${card.pv}pv`] + 1 : 1;
+
+    let eEffect = document.createElement("div");
+    eEffect.classList.add("effect");
+    for (const key in card.effect) {
+        const item = card.effect[key];
+        let eItem = document.createElement("div");
+        eItem.classList.add("item");
+        eItem.classList.add(item);
+        eItem.innerText = item;
+        eEffect.appendChild(eItem);
+    }
+    eCard.appendChild(eEffect);
+    return eCard;
+}
